645e0efd2d38de48ae37b1be1f21408a506f29da
ewsnybchan7 <50603204+ewsnybchan7@users.noreply.github.com>
2020-12-14 01:04:57 +0900

    fewa
    fewa

--- Assets/Scripts/Network/NetworkSample.cs
+++ Assets/Scripts/Network/NetworkSample.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class NetworkSample : MonoBehaviourPunCallbacks
 {
+    public Transform[] spawnPositions;
+
     private void Awake()
     {
-        Screen.SetResolution(960, 540, false);
-        PhotonNetwork.ConnectUsingSettings();
+        //Screen.SetResolution(960, 540, false);
+        //PhotonNetwork.ConnectUsingSettings();
     }
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Hashtable playerProperty = PhotonNetwork.LocalPlayer.CustomProperties;
+
+        int i = (int)playerProperty["Number"];
+        PhotonNetwork.Instantiate("NetworkTank", spawnPositions[i].position, spawnPositions[i].rotation)
+            .GetComponent<NetworkPlayer>().m_PlayerColor = (Color)playerProperty["Color"];
     }
 
     // Update is called once per frame
@@ -29,7 +34,7 @@ public class NetworkSample : MonoBehaviourPunCallbacks
     #region Photon Callbacks
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, null);
+        //PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, null);
     }
 
     public override void OnJoinedRoom()
