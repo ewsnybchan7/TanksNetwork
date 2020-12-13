1fd2ece74f02250a90704007bea268a0437c7839
RushBsite <zabaniya09@gmail.com>
2020-12-13 22:44:04 +0900

    include AI_SCRIPT

---
+++ Assets/Scripts/Enemy/Bomber_movement_AI.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
+
+
+public class Bomber_movement_AI : FSM
+{
+    public GameObject Bomb;
+    public BombExplosion TankExplosion;
+    public TankHealth tankHealth;
+    public GameObject Patrol_icon;
+    public GameObject Attack_icon;
+    public GameObject Chase_icon;
+    //NPC 사망판정
+    private bool isDead = false;
+    private float elapsedTime = 0.0f;
+    private float shootRate = 3.0f;
+
+    private GameObject player = null;
+    private NavMeshAgent navMeshAgent;
+
+    private Animator BombAnimator;
+
+    private Renderer iconColorP; // 패트롤 아이콘 컬러 렌더러
+    private Renderer iconColorA; // 어택 아이콘 컬러 렌더러
+    private Renderer iconColorC; // 추적 아이콘 컬러 렌더러
+
+    private int count = 0;
+
+    public enum FSMState
+    {
+        None,
+        Chase,
+        Attack,
+        Dead,
+    }
+
+    //현재 NPC state
+    public FSMState m_CurState;
+
+
+
+    //Initialize the Finite state machine for the NPC tank
+    protected override void Initialize()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        BombAnimator = Bomb.GetComponent<Animator>();
+
+        iconColorP = Patrol_icon.GetComponent<Renderer>(); // 눈모양 아이콘의 렌더러 연결
+        iconColorA = Attack_icon.GetComponent<Renderer>(); // 눈모양 아이콘의 렌더러 연결
+        iconColorC = Chase_icon.GetComponent<Renderer>(); // 눈모양 아이콘의 렌더러 연결
+
+    }
+    //Update each frame
+    protected override void FSMUpdate()
+    {
+        switch (m_CurState)
+        {
+            case FSMState.Chase: UpdateChaseState(); break;
+            case FSMState.Attack: UpdateAttackState(); break;
+            case FSMState.Dead: UpdateDeadState(); break;
+        }
+
+        //시간 업데이트
+        elapsedTime += Time.deltaTime;
+
+        //체력이 없으면 dead 상태로 
+        if (this.tankHealth.getCurHealth() <= 0)
+            m_CurState = FSMState.Dead;
+    }
+
+    private void UpdateDeadState()
+    {
+        if (!isDead)
+        {
+            Debug.Log("Dead");
+        }
+    }
+
+    private void UpdateAttackState()
+    {
+
+        iconColorP.material.color = new Color(1, 1, 1, 0);
+        iconColorA.material.color = new Color(1, 1, 1, 1);
+        iconColorC.material.color = new Color(1, 1, 1, 0);
+
+        BombAnimator.SetBool("isDetonate", true);
+        
+        Collider[] players = Physics.OverlapSphere(transform.position, 5.0f, LayerMask.GetMask("Players"));
+        if(players.Length > 0)
+            player = players[0].gameObject;
+        else
+        {
+            navMeshAgent.enabled = true;
+            m_CurState = FSMState.Chase;
+            return;
+        }
+
+        Vector3 _direction = (player.transform.position - transform.position).normalized;
+        Quaternion _lookRotation = Quaternion.LookRotation(_direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3);
+        if (count == 0)
+        {
+            this.TankExplosion.inite();
+            count++;
+        }
+            
+           
+        
+    }
+
+    private void UpdateChaseState()
+    {
+        iconColorP.material.color = new Color(1, 1, 1, 0); // 알파값 0 
+        iconColorA.material.color = new Color(1, 1, 1, 0); // 알파값 0 
+        iconColorC.material.color = new Color(1, 1, 1, 1);
+
+        Collider[] players = Physics.OverlapSphere(transform.position, 3.0f, LayerMask.GetMask("Players"));
+        
+        if (players.Length > 0)
+        {
+            m_CurState = FSMState.Attack;
+            player = players[0].gameObject;
+            navMeshAgent.enabled = false;
+            return;
+        }
+        else 
+        {
+            m_CurState = FSMState.Chase;
+        }
+       
+
+        player = GameObject.FindGameObjectWithTag("usr");
+
+        navMeshAgent.destination = player.GetComponent<Transform>().position;
+    }
+
+    protected bool IsInCurrentRange(Vector3 pos) //범위 내에 있는지 계산
+    {
+        float xPos = Mathf.Abs(pos.x - transform.position.x);
+        float zPos = Mathf.Abs(pos.z - transform.position.z);
+
+        if (xPos <= 5 && zPos <= 5)
+            return true;
+
+        return false;
+    }
+
+
+}
--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -12,7 +12,8 @@ public class TankShooting : MonoBehaviour
     public AudioClip m_FireClip;         
     public float m_MinLaunchForce = 15f; 
     public float m_MaxLaunchForce = 30f; 
-    public float m_MaxChargeTime = 0.75f;
+    public float m_MaxChargeTime = 0.75f; //min 차지에서 max 차지까지 걸리는 시간
+    public bool m_IsAI;
 
     
     private string m_FireButton;         
@@ -30,59 +31,82 @@ public class TankShooting : MonoBehaviour
 
     private void Start()
     {
-        m_FireButton = "Fire" + m_PlayerNumber;
+        m_FireButton = "Fire" + m_PlayerNumber; //몇번째 플레이어 의 fire 버튼인지 받음
 
-        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime; //차징 속도 계산
     }
     
 
     private void Update()
     {
+        if (m_IsAI)
+        {
+            return;
+        }
         // Track the current state of the fire button and make decisions based on the current launch force.
+        // 에임 슬라이더 디폴트 값으로 설정
         m_AimSlider.value = m_MinLaunchForce;
 
-        if(m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        // If the max force has been exceeded and the shell hasn't yet been launched...
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
         {
-            // at max charge, not yet fired
+            // ... use the max force and launch the shell.
             m_CurrentLaunchForce = m_MaxLaunchForce;
             Fire();
         }
+        // Otherwise, fire 버튼 눌렸을때(눌린상태)(처음누른상태 진입 체크)
         else if (Input.GetButtonDown(m_FireButton))
         {
-            // have we pressed fire for the first time?
+            // ... reset the fired flag and reset the launch force.
             m_Fired = false;
             m_CurrentLaunchForce = m_MinLaunchForce;
 
-            m_ShootingAudio.clip = m_ChargingClip;
+            // Change the clip to the charging clip and start it playing.
+            m_ShootingAudio.clip = m_ChargingClip; //차징 클립 재생
             m_ShootingAudio.Play();
         }
-        else if(Input.GetButton(m_FireButton) && !m_Fired)
+        // Otherwise, 버튼 눌린(홀드) 상태에서 발사 안됬을 경우
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
         {
-            // Holding the fire button, not yet fired
+            // Increment the launch force and update the slider.
             m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
             m_AimSlider.value = m_CurrentLaunchForce;
         }
-        else if(Input.GetButtonUp(m_FireButton) && !m_Fired)
+        // Otherwise, 버튼 릴리스 상태에서 발사 안됬을 경우
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
         {
-            // we released the button, having not fired yet
+            // ... launch the shell.
             Fire();
         }
     }
 
 
-    private void Fire()
+    public void Fire()
     {
         // Instantiate and launch the shell.
+        // Set the fired flag so only Fire is only called once.
         m_Fired = true;
 
-        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
-        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        //AI 라면 평균값으로 shell 발사
+        if (m_IsAI)
+        {
+            m_CurrentLaunchForce = m_MaxLaunchForce / 2.0f;
+        }
 
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; ;
+
+        // Change the clip to the firing clip and play it.
         m_ShootingAudio.clip = m_FireClip;
         m_ShootingAudio.Play();
 
+        // Reset the launch force.  This is a precaution in case of missing button events.
         m_CurrentLaunchForce = m_MinLaunchForce;
+
     }
 }
