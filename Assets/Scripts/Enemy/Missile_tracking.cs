2c626382d5bf4e32368fa7f69154b2d9947a1fa0
RushBsite <zabaniya09@gmail.com>
2020-12-14 17:19:53 +0900

    include missile tracking
    proto type missile

---
+++ Assets/Scripts/Enemy/Missile_tracking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile_tracking : MonoBehaviour
+{
+    //proto type
+    private Transform missileTarget;
+    public Rigidbody missileRigidbody;
+
+    public float turn;
+    public float missilseVelocity;
+    private float elasped = 0;
+
+    private void FixedUpdate()
+    {
+        elasped += Time.deltaTime;
+
+
+        GameObject target = GameObject.FindGameObjectWithTag("usr");
+        missileTarget = target.GetComponent<Transform>();
+        if (elasped > 0.5f)
+            missileRigidbody.velocity = transform.up * missilseVelocity;
+        else
+        {
+            missileRigidbody.velocity = transform.forward * missilseVelocity;
+
+        }
+
+        var missileTargetRotation = Quaternion.LookRotation(missileTarget.position - transform.position);
+
+        missileRigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, missileTargetRotation, turn));
+    }
+}
